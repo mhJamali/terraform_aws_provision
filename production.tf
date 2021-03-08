c11870aeed2b78e970af22ccebb5d5bdc334d06b
mhjamali <haquejamali@gmail.com>
2021-03-08 13:24:11 +0530

    modified

--- production.tf
+++ production.tf
@@ -13,7 +13,7 @@ module "networking" {
   region               = var.region
   environment          = var.environment
   vpc_cidr             = var.vpc_cidr
-  public_subnets_cidr  = "${var.public_subnets_cidr
+  public_subnets_cidr  = var.public_subnets_cidr
   private_subnets_cidr = var.private_subnets_cidr
   availability_zones   = local.production_availability_zones
 }
